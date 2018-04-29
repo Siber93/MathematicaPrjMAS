563ab1fa7bdc70c6f5dbbd67cbbcd404a333f4d0
Simone Bertolini <siber93@hotmail.it>
2018-04-29 14:53:20 +0200

    Costruito scheletro package
    Costruito scheletro package con commenti iniziali ed intestazione

--- Package.m
+++ Package.m
@@ -1,3 +1,43 @@
 (* ::Package:: *)
 
+(* ::Code::Initialization::Plain:: *)
+(* PACKAGE.M
+ * Progetto d'esame di Matematica Computazionale + Calcolo Numerico e Software Didattico
+ * Corsi di laurea magistrale in Informatica e Matematica
+ * Anno accademico 2017/2018
+ * 
+ * Autori:
+ *   Simone Bertolini, Alessio Francesconi, Matteo Grillini
+ *
+ * Mathematica Version:
+ * 	 11.1
+ *)
+ 
+BeginPackage[ "MClib`"];
 
+	(* Package Beginning*)
+
+	Unprotect["MClib`*"] (* Remove functions redefinition protection *)
+	ClearAll["MClib`*"];
+	
+	(* Usage Definitions *)
+	
+	
+	
+	(* End of Usage Definitions *)
+	
+	Begin["`Private`"]; 
+	
+		(* Beginning of Private functions definition space *)
+		
+		
+		
+		(* End of Private functions definition space *)
+		
+	End[];
+	
+	Protect["MClib`*"] (* Protect package's names *)
+	
+	(* End of Package *)
+	
+EndPackage[];
