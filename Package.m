78d4d08f2d0250439099094194cda2155492f6f0
Simone Bertolini <siber93@hotmail.it>
2018-05-05 10:52:52 +0200

    Modificata visualizzazione funzioni inserite + Modifiche Package

--- Package.m
+++ Package.m
@@ -30,7 +30,28 @@ BeginPackage[ "MClib`"];
 	
 		(* Beginning of Private functions definition space *)
 		
-		
+		(* Print a form with all the functions that the teacher is inserting in the list l_ *)
+		d[l_] := Panel[
+					Grid[
+						Table[
+							With[
+								{i = i, j = j}, 
+								Row[
+									{l[[i]], 
+									"\t", 
+									Button["X", l = Delete[l, Position[l, l[[i]]]]]}
+								]
+							], 
+							{i, Length[l]},
+							{j, 1}
+						], 
+						Alignment -> Right
+					]
+				];
+				
+				
+		(* Allow to function d to ovverride l value with the value of the given variable *)
+		SetAttributes[d, HoldAll];
 		
 		(* End of Private functions definition space *)
 		
