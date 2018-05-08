8bdf5da39c8026d477726a8f1c8d78dae1d83cf4
Simone Bertolini <siber93@hotmail.it>
2018-05-08 23:09:37 +0200

    Mergiati file student vecchio-nuovo

--- Package.m
+++ Package.m
@@ -21,14 +21,41 @@ BeginPackage[ "MClib`"];
 	ClearAll["MClib`*"];
 	
 	(* Usage Definitions *)
+		p1::usage="[G1, q=0] Variable that indicates that user has already chosen something"
+		p2::usage="[G1, m!=0] Variable that indicates that user has already chosen something"
+		p3::usage="[G1, m=0] Variable that indicates that user has already chosen something"
+
 	
 	
 	
+		hyperText::usage = "Creating a cliccable text. The click trig a MessageDialog
+	 \n@txt@ Text to show
+	 \n@msg@ Message in the MessageDialog
+	 \n@curs@ Cursor type on mouse over"
+
+		d::usage = "Print a form with all the functions that the teacher is inserting in the list
+	 \n@l_@ List to analize"
 	(* End of Usage Definitions *)
 	
 	Begin["`Private`"]; 
 	
 		(* Beginning of Private functions definition space *)
+		(* Variables *)
+		(* /Variables *)
+		p1 = 0(* Variable that indicates that user has already chosen something [G1, q=0] *)
+		p2 = 0(* Variable that indicates that user has already chosen something [G1, m!=0] *)
+		p3 = 0(* Variable that indicates that user has already chosen something [G1, m=0] *)
+
+		(* Functions *)
+		
+		(* Creating a cliccable text *)
+		hyperText[txt_,msg_, curs_] := EventHandler[
+			MouseAppearance[
+				Style[txt, FontColor -> Blue], 
+				curs
+			], 
+			{"MouseClicked" :>  MessageDialog[msg]}
+		]; 
 		
 		(* Print a form with all the functions that the teacher is inserting in the list l_ *)
 		d[l_] := Panel[
@@ -51,14 +78,21 @@ BeginPackage[ "MClib`"];
 				
 				
 		(* Allow to function d to ovverride l value with the value of the given variable *)
-		SetAttributes[d, HoldAll];
+		SetAttributes[d, HoldAll];	
 		
+		(* Functions *)
 		(* End of Private functions definition space *)
 		
 	End[];
 	
-	Protect["MClib`*"] (* Protect package's names *)
+	(*Protect["MClib`*"]*) (* Protect package's names *)
 	
 	(* End of Package *)
 	
 EndPackage[];
+
+
+
+
+
+
